5acdcd7fbf2a46472104229740e1da156d557a99
CliffGreene <lifeguardtwenty@gmail.com>
2020-02-06 02:37:08 +0000

    Moving files around refactoring to get rid of sql injection points

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,17 +10,23 @@ const pool = new Pool({
 
 //pool.connect();
 
-pool.query(`
+const cohortName = process.argv[2] || 'JUL02';
+const values = [cohortName];
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
-.then(res => {
+`
+
+pool.query(queryString, values).then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+  
+}).catch(err => console.error('query error', err.stack));
+
